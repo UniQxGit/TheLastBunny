5464447e72d593126bc9a26a22012cf407608600
UniQxGit <hyunmail94@gmail.com>
2018-04-17 21:51:12 -0700

    sprite script update

--- sprites.py
+++ sprites.py
@@ -8,6 +8,15 @@ gems_path = "Assets/Characters/"
 class Sprite:
 	rect = None
 	base_image = None
+	
+	#base_animation is the folder of animtion i.e "attack".
+	#it will look in the folder, and load all frames for parameter "length"
+	#the final path will be "base_animation+name+i" for each i to length
+	def __init__(self,base_animation,name,length):
+		for i in range(length):
+			
 
-	def __init__(self,base_animation,length):
-		
+	#Add function for adding in an animation
+	#it will look in the folder, and load all frames for parameter "length"
+	#the final path will be "base_animation+name+i" for each i to length
+	#def add_anim(self,animation,length)
